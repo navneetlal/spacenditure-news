4bdd49e81503e58aebcab97625e06447a5dc6d67
Navneet Lal Gupta <navneetlalg@gmail.com>
2020-03-23 12:24:01 +0530

    under construction added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c31cc6f5-cb8b-4cfb-b201-f03da86c3dad/deploy-status)](https://app.netlify.com/sites/objective-lovelace-0ea573/deploys)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 
-import HeaderComponent from './Component/Header';
+import HeaderComponent from "./Component/Header";
 
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-import Cyan from '@material-ui/core/colors/cyan';
+import Cyan from "@material-ui/core/colors/cyan";
+import { Typography, Box } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  mainContainer: {
-    backgroundColor: Cyan[50],
-    height: '100vh'
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      marginTop: theme.spacing(2),
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column"
+    },
+    flexItemsSpacing: {
+      marginTop: theme.spacing(2),
+      color: theme.palette.primary.dark
+    }
+  })
+);
 
 function App() {
   const classes = useStyles();
   return (
     <Container maxWidth="lg" className={classes.mainContainer}>
-      <HeaderComponent />
+      <img src="logo.jpeg" height="100" width="100" />
+      <Typography
+        variant="h3"
+        component="h1"
+        className={classes.flexItemsSpacing}
+      >
+        <Box fontWeight="fontWeightBold" m={1} letterSpacing={3}>
+          SPACENDITURE
+        </Box>
+      </Typography>
+      <Typography variant="h5">
+        <Box fontWeight="fontWeightLight" letterSpacing={6}>
+          Towards New Adventures
+        </Box>
+        </Typography>
+        <Typography variant="h5">
+        <Box fontWeight="fontWeightLight" m={3} letterSpacing={12}>
+          &bull;&bull;&bull;
+        </Box>
+        </Typography>
+      <Typography>
+        Get latest updated news related on space.
+      </Typography>
+      <img src="website-under-construction.jpg" width="500" />
+      {/* <HeaderComponent /> */}
     </Container>
   );
 }
